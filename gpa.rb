42f783a5cd60f4512afd5e46d3c2d7579342c3de
karrde00 <karrde00@gmail.com>
2013-04-15 20:59:11 -0500

    Program functions

--- gpa.rb
+++ gpa.rb
@@ -1,13 +1,30 @@
-require_relative 'gpaMethods'
 require_relative 'gpaClasses.rb'
 
 
-#Defines variables required through the entire app
-$run = true
-$courses = [] # Used to store all courses entered
-
+student = Student.new("Frank")
 # Starts the program
-while $run 
- menu
+run = true
+while run
+  options = {one: "1. Add Course", two: "2. Delete Course", three: "3. Calclate GPA", 
+               four: "4. Display Courses", five: "5. Exit"}
+  puts ("What would you like to do?") 
+  options.each{|key, value| puts value}
+  response = gets.chomp
+  puts
+
+  case response.to_i
+    when 1 # Add Course
+      student.add_class(Course.new)
+    when 2 # Delete Course
+     student.delete_class
+    when 3 # Calculate GPA
+      puts student.calculate_gpa
+    when 4 # Display Courses
+     schedule = student.display
+    when 5 # Exit
+      run = false
+    else 
+      puts("Not a Valid choice\n")
+    end
 end
 
--- gpaClasses.rb
+++ gpaClasses.rb
@@ -1,24 +1,84 @@
-class Course
-	attr_accessor :name, :creditHours, :grade, :courses
-  
-  def initialize(name)
-      @name = name
-      $courses.push(@name.capitalize)
-	  add
+class Student
+  attr_accessor :courses
+
+  def initialize name
+    @name = name
+    @courses = []
   end
 
-  def add   
+  def add_class course
+    @courses << course
+  end
+
+  def delete_class
+    puts "What course would you like to delete?"
+    name = gets.chomp
+    name = name.capitalize
+
+    @courses.each do |course|
+      if course.instance_variable_get(:@name) == name
+        @courses.delete(course)
+        puts "#{name} has been deleted!"
+      else
+        puts "This courses does not exist"
+      end
+    end
+  end
+
+
+  def display
+    @courses.each do |course|
+      p course
+    end
+  end
+
+  def calculate_gpa 
+    points, hours = 0, 0
+    @courses.each do |course|
+      points = points + course.instance_variable_get(:@gradePoints)
+      hours  = hours  + course.instance_variable_get(:@creditHours)
+    end
+
+    gpa = points / hours
+
+    puts "Your GPA is: #{gpa}"
+
+
+  end
+
+end
+
+
+
+class Course
+	attr_accessor :creditHours, :grade, :gradePoints
+  
+  def initialize
+      puts "What is the name of the course?"
+      @name = gets.chomp
+      @name = @name.capitalize
       puts("What grade did you receive?")
       @grade = gets.chomp
-      $courses.push(@grade.upcase!)
-      
+      @grade = @grade.upcase
       puts("How many credit hours was the course?")
       @creditHours = gets.chomp
-      $courses.push(@creditHours)
+      @creditHours = @creditHours.to_i
+
+      grade_conversion
   end
 
-
-  def delete
-
+  def grade_conversion
+    case @grade.upcase
+    when 'A'
+      @gradePoints = 4.0 * @creditHours
+    when 'B'
+      @gradePoints = 3.0 * @creditHours
+    when 'C'
+      @gradePoints = 2.0 * @creditHours
+    when 'D'
+      @gradePoints = 1.0 * @creditHours
+    else
+      @gradePoints = 0.0
+    end
   end
 end
